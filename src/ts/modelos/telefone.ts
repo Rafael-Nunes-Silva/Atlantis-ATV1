c4fdc761385311b5458e91b6734eb950d86e5601
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-03-02 11:19:14 -0300

    Interface Protoripo implmentada para a classe Telefone

--- src/ts/modelos/telefone.ts
+++ src/ts/modelos/telefone.ts
@@ -1,4 +1,13 @@
-export default class Telefone {
+import Prototipo from "../interfaces/prototipo";
+
+export default class Telefone implements Prototipo {
     public ddd: string;
     public numero: string;
+
+    public clonar(): Prototipo {
+        const novoTelefone = new Telefone();
+        novoTelefone.ddd = this.ddd;
+        novoTelefone.numero = this.numero;
+        return novoTelefone;
+    }
 }
